9ddcb3bd7727447ae87c0fcc72c628df0c70eb1e
patelvk2000 <patelvk2000@gmail.com>
2024-06-09 08:14:12 +0000

    Added dots in bg

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,34 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="text-center flex flex-col gap-4 min-h-screen mx-auto justify-center bg-[#242424] text-white">
+    <div className="w-full text-center flex flex-col gap-4 min-h-screen mx-auto relative justify-center bg-[#242424] bg-dot-white/[0.2] text-white">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#242424] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="w-full flex flex-row gap-4 items-center justify-center">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="h-28 hover:drop-shadow-[0_0_2em_#646cffaa] p-2" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="h-16 sm:h-28 hover:drop-shadow-[0_0_2em_#646cffaa] p-2"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="h-28 hover:drop-shadow-[0_0_2em_#61dafbaa] p-2" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="h-16 sm:h-28 hover:drop-shadow-[0_0_2em_#61dafbaa] p-2"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1 className="text-7xl font-bold font-mono">Vite + React</h1>
-      <div className="card">
+      <h1 className="text-3xl sm:text-7xl font-bold font-mono">Vite + React</h1>
+      <div>
         <button
-          className="px-3 py-4 rounded-lg border-2 border-white bg-black hover:border-[#646cffaa]"
+          className="px-4 py-2 rounded-lg border-2 border-white bg-black hover:border-[#646cffaa]"
           onClick={() => setCount((count) => count + 1)}
         >
           count is {count}
         </button>
-        <p>
+        <p className="mt-4">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
